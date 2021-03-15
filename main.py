44ee3218d2f91e4f4d8bde2d27d8f059903495e9
jihun04 <yujihun20060217@gmail.com>
2021-03-15 21:46:57 +0900

    Update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import csv
 
 KEYWORD = "buy domain"
 
@@ -17,21 +16,14 @@ search_bar = browser.find_element_by_class_name("gLFyf")
 search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
+search_results = browser.find_elements_by_class_name("g")
+
 shitty_element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "mnr-c"))
+    EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
 )
 
-print(shitty_element)
-
-""" search_results = browser.find_elements_by_class_name("g")
-file = open("titles.csv", mode="w")
-writer = csv.writer(file)
 for index, search_result in enumerate(search_results):
-    class_name = search_result.get_attribute("class")
-    if "kno-kp mnr-c g-blk" not in class_name:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-        title = search_result.find_element_by_tag_name("h3")
-        if title:
-            writer.writerow([title.text])
- """
+    if "kno-kp mnr-c g-blk" not in search_result.get_attribute("class")
+    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+
 browser.quit()
