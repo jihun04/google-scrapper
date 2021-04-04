2fd22f16d2976c73a1f66217d5788ccb66d3f01a
jihun04 <yujihun20060217@gmail.com>
2021-04-04 14:30:36 +0900

    Refactor

--- responsive.py
+++ responsive.py
@@ -1,21 +1,54 @@
-import time
-from math import ceil
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
+from math import ceil
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
 
-browser.get("https://nomadcoders.co")
+class ResponsiveTester():
+    def __init__(self, urls):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.maximize_window()
+        self.urls = urls
+        self.sizes = [480, 960, 1360, 1920]
 
-sizes = [480, 960, 1360, 1920]
-browser_height = browser.get_window_size().get("height")
+    def screenshots(self, url):
+        file_name = url.replace("www.", "", 1).split("//")[1].split(".")[0]
+        if not os.path.isdir(file_name):
+            os.mkdir(file_name)
+        self.browser.get(url)
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_all_elements_located((By.TAG_NAME, "html"))
+        )
+        browser_outer_height = self.browser.get_window_size().get("height")
+        browser_inner_height = self.browser.execute_script(
+            "return window.innerHeight")
+        for size in self.sizes:
+            self.browser.set_window_size(size, browser_outer_height)
+            scroll_size = self.browser.execute_script(
+                "return document.body.scrollHeight")
+            total_sections = ceil(scroll_size / browser_inner_height)
+            for section in range(total_sections):
+                self.browser.execute_script(
+                    f"window.scroll(0, {browser_inner_height*section})")
+                time.sleep(0.7)
+                self.browser.save_screenshot(
+                    f"{file_name}/{size}x{section+1}.png")
 
-for size in sizes:
-    browser.set_window_size(size, browser_height)
-    time.sleep(2)
-    scroll_size = browser.execute_script("return document.body.scrollHeight")
-    total_sections = ceil(scroll_size / browser_height)
-    for section in range(total_sections):
-        browser.execute_script(
-            f"window.scroll(0, {browser_height*section})")
-        time.sleep(2)
+    def start(self):
+        for url in self.urls:
+            self.screenshots(url)
+
+    def finish(self):
+        self.browser.quit()
+
+    def run(self):
+        self.start()
+        self.finish()
+
+
+tester = ResponsiveTester([])
+tester.run()
